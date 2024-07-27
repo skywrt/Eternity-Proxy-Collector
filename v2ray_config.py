2c6657ba1876d3e20ee07f10761a4512bf02e123
skywrt <156793219+skywrt@users.noreply.github.com>
2024-07-28 00:29:38 +0800

    Update v2ray_config.py

--- v2ray_config.py
+++ v2ray_config.py
@@ -1,6 +1,7 @@
 import base64
 
 def generate_v2rayn_base64(nodes, output_file):
-    encoded_nodes = [base64.b64encode(node.encode('utf-8')).decode('utf-8') for node in nodes]
     with open(output_file, 'w') as f:
-        f.write('\n'.join(encoded_nodes))
+        for node in nodes:
+            encoded_node = base64.b64encode(node.encode('utf-8')).decode('utf-8')
+            f.write(encoded_node + '\n')
