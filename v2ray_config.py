14f60f046663d8e829fa484a97bde4a6361c0f85
skywrt <156793219+skywrt@users.noreply.github.com>
2024-07-28 02:08:25 +0800

    Update v2ray_config.py

--- v2ray_config.py
+++ v2ray_config.py
@@ -1,11 +1,13 @@
 import base64
 import json
+import binascii  # 添加导入
 
 def decode_base64(encoded):
     decoded = ''
+    encoded_bytes = encoded.encode('utf-8')  # 将字符串转换为字节类型
     for encoding in ['utf-8', 'iso-8859-1']:
         try:
-            decoded = base64.urlsafe_b64decode(encoded + b'=' * (-len(encoded) % 4)).decode(encoding)
+            decoded = base64.urlsafe_b64decode(encoded_bytes + b'=' * (-len(encoded_bytes) % 4)).decode(encoding)
             break
         except (UnicodeDecodeError, binascii.Error):
             pass
